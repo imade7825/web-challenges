9fbb97bddde2b03dc2c0382f8a84ff7c9b93f806
Imade <elbahit@gmx.de>
2025-06-17 11:46:25 +0200

    solution input and strings switch values

--- JavaScript/js-inputs-and-strings/js-inputs-and-strings_switch-values/js/index.js
+++ JavaScript/js-inputs-and-strings/js-inputs-and-strings_switch-values/js/index.js
@@ -1,13 +1,19 @@
 console.clear();
 
 const firstInput = document.querySelector('[data-js="first-input"]');
+const secondInput = document.querySelector('[data-js="second-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
-
+const switchcaseButton = document.querySelector('[data-js="button-switch"]');
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
 });
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+});
+switchcaseButton.addEventListener("click", () => {
+  const oldFirsInput = firstInput.value;
+  firstInput.value = secondInput.value;
+  secondInput.value = oldFirsInput;
 });
