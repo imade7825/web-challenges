4c23590d469482e1c89b446b040337aa5fe00a64
Imade <elbahit@gmx.de>
2025-06-17 16:21:51 +0200

    Add 3 challenge folders und solution for forms handle forms submit

---
+++ JavaScript/js-forms/js-forms_handle-form-submit/js/index.js
@@ -0,0 +1,16 @@
+console.clear();
+
+const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+  console.log(Number(data.age) + Number(data.badness));
+
+  form.reset();
+  document.getElementById("first-name").focus();
+});
