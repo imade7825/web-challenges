e8e1e6bdbf575aa1ee5bd35eacf5703099e21cbd
Imade <elbahit@gmx.de>
2025-06-18 11:56:47 +0200

    Add challenge folders for js create element

--- JavaScript/js-forms/js-forms_checkbox-input/js/index.js
+++ JavaScript/js-forms/js-forms_checkbox-input/js/index.js
@@ -12,13 +12,25 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+tosCheckbox.addEventListener("change", () => {
+  if (tosCheckbox.checked) {
+    hideTosError("tos-error");
+  } else showTosError("tos-error");
+});
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+  if (tosCheckbox.checked) {
+    return alert("Form submitted");
+  } else {
+    return showTosError("tos-error");
+  }
 
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
+
+  // alert("Form submitted");
 });
