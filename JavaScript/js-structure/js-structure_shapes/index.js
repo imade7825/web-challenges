b6f06c73e64010e3b07223f567311977a6818292
Imade <elbahit@gmx.de>
2025-06-24 16:24:59 +0200

    new solution to shape challenge

--- JavaScript/js-structure/js-structure_shapes/index.js
+++ JavaScript/js-structure/js-structure_shapes/index.js
@@ -1,27 +1,16 @@
 console.clear();
 import getRandomColor from "./utils/randomColor.js";
+import Circle from "./components/Circle/Circle.js";
+import Square from "./components/Square/Square.js";
+import Pentagon from "./components/Pentagon/Pentagon.js";
 
 const root = document.getElementById("root");
+const circle = Circle();
+const square = Square();
+const pentagon = Pentagon();
 
-const circle = document.createElement("div");
-circle.classList.add("circle");
-circle.addEventListener("click", () => {
-  circle.style.backgroundColor = getRandomColor();
-  return circle;
-});
 
-const square = document.createElement("div");
-square.classList.add("square");
-square.addEventListener("click", () => {
-  square.style.backgroundColor = getRandomColor();
-  return square;
-});
 
-const pentagon = document.createElement("div");
-pentagon.classList.add("pentagon");
-pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = getRandomColor();
-  return pentagon;
-});
+
 
 root.append(circle, square, pentagon);
